c55f9898398c35b880c7adf0b50499d3be4d3ed5
buffBoi <nataliemurphy500@gmail.com>
2023-06-08 13:32:34 -0700

    trending section gird built

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { prisma } from "app/api/client";
+import Trending from "./(home)/Trending";
 
 //server compoqnet to get data from db
 const getPosts = async () => {
@@ -8,8 +9,21 @@ const getPosts = async () => {
 };
 
 async function Home() {
-  const posts = await getPosts();
-  console.log("posts", posts);
-  return <main>Hello</main>;
+  //const posts = await getPosts();
+  //console.log("posts", posts);
+  return (
+    <main className='px-10 leading-7'>
+      <Trending />
+      <div className='md:flex gap-10 mb-5'>
+        <div className='basis-3/4'>
+          {/* <Tech techPosts={techPosts} />
+          <Travel travelPosts={travelPosts} />
+          <Other otherPosts={otherPosts} /> */}
+          <div className='hidden md:block'>{/* <Subscribe /> */}</div>
+        </div>
+        <div className='basis-1/4'>{/* <Sidebar /> */}</div>
+      </div>
+    </main>
+  );
 }
 export default Home;
